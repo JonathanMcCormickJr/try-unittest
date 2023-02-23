a806cf967eb358e05d1454d82aef315f0a6ae78d
Jonathan A. McCormick, Jr <67705789+LiberlandHacker@users.noreply.github.com>
2023-02-23 03:47:42 +0000

    Have 2 placeholder tests.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import unittest
 
+class MyFirstPythonTest(unittest.TestCase):
+    def test_function_0(self):
+        pass
+
+    def test_function_1(self):
+        pass
+
 if __name__ == "__main__":
     unittest.main()
