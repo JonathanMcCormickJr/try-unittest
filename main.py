35231f1cae77dece3e2ca45e9f8b0a2988f9037f
Jonathan A. McCormick, Jr <67705789+LiberlandHacker@users.noreply.github.com>
2023-02-23 03:55:00 +0000

    Regroup tests into multiple classes.

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ class MyFirstPythonTest(unittest.TestCase):
     def test_function_0(self):
         pass
 
-    def mytest_function_1(self):
+    def test_function_1(self):
+        pass
+        
+class MySecondPythonTest(unittest.TestCase):
+    def test_function_0(self):
         pass
 
+    def test_function_1(self):
+        pass
+        
 if __name__ == "__main__":
     unittest.main()
